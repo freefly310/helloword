e80e59a6cb5f824b16a6060f64ca13fe62fb3bc5
Bob <Bob@qq.com>
2023-12-26 17:42:46 +0800

    111

--- src/main/java/com/bob/demo/controller/HelloWorld.java
+++ src/main/java/com/bob/demo/controller/HelloWorld.java
@@ -1,2 +1,15 @@
-package com.bob.demo.controller;public class HelloWorld {
+package com.bob.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web/")
+public class HelloWorld {
+
+        @RequestMapping("/hello")
+        public String hello() {
+            return "Hello World!!!";
+        }
 }
